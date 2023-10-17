bfbb1a86324e1ef9bf8666cfd2437219ed826f33
Gabin LOTZ <47774056+heyimgab@users.noreply.github.com>
2023-10-17 12:03:21 +0200

    push

--- TD/dcoupnb.c
+++ TD/dcoupnb.c
@@ -2,5 +2,21 @@
 #include <stdlib.h>
 
 int main(){
-
+    int nombre = 0;
+    int centaines = 0;
+    int dizaines = 0;
+    int unites = 0;
+    int reste = 0;
+    printf("Choissisez un nombre entre 1 et 999\t");
+    scanf("%d", &nombre);
+    if(nombre<1 && nombre>999) {
+        printf("Le nombre n'est pas valide");
+    } else {
+        centaines = nombre / 100;
+        reste = nombre % 100;
+        dizaines = reste / 100;
+        unites = reste % 100;
+    }
+    printf("Le nombre de centaines est de:%d, de dizaines est:%d, et d'unités:%d\n", centaines, dizaines, unites);
+    return 0;
 }
--- TD/volume_cylindre.c
+++ TD/volume_cylindre.c
@@ -7,7 +7,9 @@ int main(){
     float rayon = 5.2;
     float profondeur = 2.8;
     float pi = 3.14159;
+
     volume = pi * pow(rayon, 2) * profondeur;
+    
     printf("Le volume du cylindre est : %f\n", volume);
     return 0;
 }

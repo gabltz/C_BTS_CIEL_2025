778c3d32f9de8bb593edcf2db4a56f68e7980ff4
Gabin LOTZ <47774056+heyimgab@users.noreply.github.com>
2023-10-17 11:05:20 +0200

    push

--- TD/mod3.c
+++ TD/mod3.c
@@ -5,9 +5,9 @@ int main(){
     int nb = 0;
     printf("Entrez un nombre entier : ");
     scanf("%d", &nb);
-    if (nb % 3) == 0{
-        printf("L'entier est divisible par 3.");
-    };else{
-        printf("L'entier n'est pas divisible par 3.");
+    if (nb % 3 == 0){
+        printf("L'entier est divisible par 3. \n");
+    }else{
+        printf("L'entier n'est pas divisible par 3.\n");
     };
 }
